80039a81853163cc1baff73f0b75eb143f3c6293
han253 <han253@gmail.com>
2024-11-15 11:05:11 -0500

    add media conf

--- proyecto2/settings.py
+++ proyecto2/settings.py
@@ -126,6 +126,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+# config/settings.py
+MEDIA_URL = "/archivos/"
+MEDIA_ROOT = BASE_DIR / "archivos"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
