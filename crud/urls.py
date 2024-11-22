c09241d7cd9fd55a772c023fd7ec7961de26c68c
Azier <ricardoandresperezporras@gmail.com>
2024-11-22 15:13:26 -0500

    first files

--- crud/urls.py
+++ crud/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('detalle/<producto_id>/', views.detalles, name='detalleProducto'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import Productos
 # Create your views here.
 def index(request):
-    template = loader.get_template("index.html")
-    context = {}
+    lista_productos = Productos.objects.all()
+    template = loader.get_template("index.html") 
+    context = {'productos': lista_productos}
+    return HttpResponse(template.render(context,request))
+
+def detalles(request, producto_id):
+    detalles_producto = Productos.objects.get(id=producto_id)
+    template = loader.get_template("detail.html")
+    context = {'productos': detalles_producto}
     return HttpResponse(template.render(context,request))
